35a759cb5b32d06f7227f3090ea26e0cccdbb8de
Djemorin <jerome.morin@maformationenligne.bzh>
2017-04-04 17:32:01 +0200

    plus propre déjà :p

--- WWW/philosophie.php
+++ WWW/philosophie.php
@@ -9,26 +9,30 @@
 					<img src="imgs/pics/philo1.jpg">
 				</div>
 
-				<p>Nous sommes Artisan Lunetier, une profession paramédicale.</p>
+				<p>Nous sommes <strong>Artisan Lunetier</strong>, une profession paramédicale.</p>
 
 				<h3>Oui, d’accord, mais ça veut dire quoi ?</h3>
 
 				<p>Cela veut dire que nous possédons un savoir-faire et des connaissances techniques et physiologiques qui nous permettent de nous placer au service de votre vue pour réaliser l’équipement qui vous correspondra au mieux.</p>
 			</div>
-
+			<br>
 
 			<div class="p3d">
 				<h3>Comment ?</h3>
 
-				<p>Par l’approche paramédicale: nous avons à transcrire une prescription médicale en une paire de lunettes qui prend en compte vos besoins visuels en s’inscrivant dans votre quotidien avec le respect de votre façon de vivre, votre personnalité et de votre budget.</p>
+				<p><em>Par l’approche paramédicale:</em> nous avons à transcrire une prescription médicale en une paire de lunettes qui prend en compte vos besoins visuels en s’inscrivant dans votre quotidien avec le respect de votre façon de vivre, votre personnalité et de votre budget.</p>
 
-				<p>Par l’approche artisanale: nous mettons en œuvre nos connaissances techniques et notre savoir faire pour vous servir par le choix des matériaux, la qualité de réalisation et toutes les avancées technologiques que les verres peuvent vous apporter.</p>
+				<p><em>Par l’approche artisanale:</em> nous mettons en œuvre nos connaissances techniques et notre savoir faire pour vous servir par le choix des matériaux, la qualité de réalisation et toutes les avancées technologiques que les verres peuvent vous apporter.</p>
 				
+				<br>
+
 				<div id="qsn2" >
 					<img src="imgs/pics/philo2.jpg">
 				</div>
 
 				<p>Pour garder ces valeurs actives au quotidien, nous avons fait le choix de l’indépendance. Cela nous permet de ne pas être contraints d’obéir à des firmes qui nous imposeraient de se soumettre au pouvoir des finances, au détriment d’une relation plus humaine à laquelle nous tenons tout particulièrement. Mettre notre professionnalisme à votre écoute, celle de vos gouts, de vos envies, de la manière dont on peut vous faciliter la vie, améliorer votre confort visuel, vous conseiller en santé visuelle…</p>
+
+				<br>
 
 				<p>Et ceci ne s’arrête pas aux lunettes: nutrition, <a href="bienetre.php">yoga des yeux</a> …</p>
 
